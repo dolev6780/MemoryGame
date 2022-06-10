cbf6e210640aa58af486149ef3990c48552fb137
dolev6780 <73402474+dolev6780@users.noreply.github.com>
2022-06-10 18:00:29 +0300

    create project

---
+++ client/src/components/Board/Board.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+import './Board.css'
+export default function Board() {
+  return (
+<div class="grid">
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+  <div></div>
+</div>
+  )
+}
